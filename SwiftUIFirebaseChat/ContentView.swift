d6e8240d794e79612aaaa96a96a59fdf0e007bc8
Siarheimasiuk <winst73@gmail.com>
2023-11-30 14:48:08 +0300

    Initial Commit

---
+++ SwiftUIFirebaseChat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIFirebaseChat
+//
+//  Created by Sergio on 30.11.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIFirebaseChat/SwiftUIFirebaseChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFirebaseChatApp.swift
+//  SwiftUIFirebaseChat
+//
+//  Created by Sergio on 30.11.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFirebaseChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
